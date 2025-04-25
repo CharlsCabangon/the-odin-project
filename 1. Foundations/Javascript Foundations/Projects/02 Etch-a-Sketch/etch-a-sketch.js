1896bbdb309ee9883a17ede01b29ec92f259dd7c
Charls Cabangon <charlsmercabangon@gmail.com>
2025-04-25 14:55:41 +0800

    added color button functionality

--- 1. Foundations/Javascript Foundations/Projects/02 Etch-a-Sketch/etch-a-sketch.js
+++ 1. Foundations/Javascript Foundations/Projects/02 Etch-a-Sketch/etch-a-sketch.js
@@ -1,8 +1,16 @@
 
+// grid number inputs
 const inputRows = document.getElementById("input-rows");
 const inputCols = document.getElementById("input-cols");
 
+let currentColor = "#000000";
+let isDrawing = false;
+let isRandomColor = false;
+
 const gridCanvas = document.getElementById("grid-canvas");
+
+gridCanvas.addEventListener('mousedown', () => isDrawing = true);
+document.addEventListener('mouseup', () => isDrawing = false);
 
 function getRows() {
     return inputRows.value;
@@ -21,9 +29,25 @@ function generateGrid(rows, cols) {
             let createCol = document.createElement("div");
             createCol.classList.add("col");
             createRow.appendChild(createCol);
+
+            createCol.addEventListener('mouseover', () => {
+                if (isDrawing) {
+                    createCol.style.backgroundColor = currentColor;
+                }
+            })
         }
         gridCanvas.appendChild(createRow);
     }    
+}
+
+function generateRandomColors() {
+    const hex = "123456789ABCDEF";
+    let color = "#";
+
+    for (i = 0; i < 6; i++) {
+        color += hex[Math.floor(Math.random() * 16)];
+    }
+    return color;
 }
 
 document.getElementById("btn-generate-grid").addEventListener('click', () => {
@@ -34,4 +58,25 @@ document.getElementById("btn-generate-grid").addEventListener('click', () => {
     inputRows.value = '';
     inputCols.value = '';
 })
+
+document.getElementById("btn-colors").addEventListener('click', () => {
+    const inputColor = document.getElementById("color-picker");
+    currentColor = inputColor.value;
+
+    console.log(currentColor);
+})
+
+document.getElementById("btn-random-color").addEventListener('click', () => {
+    currentColor = generateRandomColors();
+
+    console.log(currentColor);
+})
+
+document.getElementById("btn-erase").addEventListener('click', () => {
+    currentColor = "#FFFFFF";
+
+    console.log(currentColor);
+})
+
+
 
