011bb35bc351c4f85c1571b79d4cc4723891e273
Charls Cabangon <charlsmercabangon@gmail.com>
2025-06-17 22:34:03 +0800

    finished the menu page

--- 3. Intmd Javascript/4. Webpack/Restaurant Page/src/scripts/menu.js
+++ 3. Intmd Javascript/4. Webpack/Restaurant Page/src/scripts/menu.js
@@ -1,11 +1,56 @@
+import imgChickenBurger from "../images/chicken-burger.png";
+
+import addIcon from "../assets/elements/add-icon.svg";
 
 export function loadMenu() {
-    const container = document.getElementById("content");
+    const content = document.getElementById("content");
 
-    const div = document.createElement("div");
-    const para = document.createElement("p");
+    const menuContainer = document.createElement("div");
+    menuContainer.className = "menu-container";
 
-    div.innerHTML = "MENUUUUUUUU!!!";
+    // MENU CARD
+    const menuCard = document.createElement("div")
+    menuCard.className = "menu__card";
 
-    container.appendChild(div);
+    // MENU CARD IMAGE
+    const menuCardImage = document.createElement("div")
+    menuCardImage.className = "menu__card-image";
+    const chickenBurger = document.createElement("img");
+    chickenBurger.src = imgChickenBurger;
+    chickenBurger.className = "menu__image";
+
+    menuCardImage.appendChild(chickenBurger);
+    menuCard.appendChild(menuCardImage);
+
+    // MENU CARD BODY
+    const menuCardBody = document.createElement("div");
+    menuCardBody.className = "menu__card-body";
+    const menuCardText = document.createElement("div");
+    menuCardText.className = "menu__card-text"
+
+    const menuCardTitle = document.createElement("p");
+    menuCardTitle.className = "menu__card-title";
+    menuCardTitle.textContent = "Chicken Burger";
+    const menuCardPrice =document.createElement("p");
+    menuCardPrice.className = "menu__card-price";
+    menuCardPrice.textContent = "110.00";
+
+    menuCardText.appendChild(menuCardTitle);
+    menuCardText.appendChild(menuCardPrice);
+    menuCardBody.appendChild(menuCardText);
+
+    const menuCardAction = document.createElement("div");
+    menuCardAction.className = "menu__card-action";
+    const btnIconAdd = document.createElement("img");
+    btnIconAdd.src = addIcon;
+    btnIconAdd.className = "btn__icon-add";
+
+    menuCardAction.appendChild(btnIconAdd)
+    menuCardBody.appendChild(menuCardAction);
+    menuCard.appendChild(menuCardBody);
+
+    // final appends
+    menuContainer.appendChild(menuCard);
+    content.appendChild(menuContainer);
+
 };
