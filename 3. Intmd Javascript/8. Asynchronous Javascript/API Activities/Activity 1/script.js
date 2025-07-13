bc6886e1571b661db593c56cb6f2d10c7d1ab08f
Charls Cabangon <charlsmercabangon@gmail.com>
2025-07-13 13:55:44 +0800

    API practice

---
+++ 3. Intmd Javascript/8. Asynchronous Javascript/API Activities/Activity 1/script.js
@@ -0,0 +1,33 @@
+const apiKey = 'QSKIgohqchgLSzSl5qV76DnlCQPJ0obB';
+
+const img = document.querySelector('img');
+
+async function getSearch() {
+  const input = document.getElementById('search-input');
+  const container = document.getElementById('gif-container');
+  const searchTerm = input.value.trim();
+
+  if (!searchTerm) return;
+
+  const response = await fetch(`https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${searchTerm}&limit=10`, {mode: 'cors'});
+  const searchData = await response.json();
+
+  container.innerHTML = '';
+
+  searchData.data.forEach(gif => {
+    const img = document.createElement('img');
+    img.src = gif.images.original.url;
+    container.appendChild(img);
+  });
+}
+
+document.getElementById('search-btn').addEventListener('click', getSearch);
+
+
+// fetch(`https://api.giphy.com/v1/gifs/translate?api_key=${apiKey}&s=${searchTerm}`, {mode: 'cors'})
+//   .then(function(response) {
+//     return response.json(); // Step 1: Parse the raw response
+//   })
+//   .then(function(response) {
+//     img.src = response.data.images.original.url; // Step 2: Use the actual parsed data
+//   });
