8ae5d8c5ed232899cf204a71d655fd92ec874042
Charls Cabangon <charlsmercabangon@gmail.com>
2025-07-30 19:45:49 +0800

    linked list

---
+++ 3. Intmd Javascript/9. Recursive Methods/Practice/practice5.js
@@ -0,0 +1,6 @@
+function sum(n, total = 0) {
+  if (n <= 0) return total;
+  return sum(n - 1, total + n);
+}
+
+console.log(sum(5));
---
+++ 3. Intmd Javascript/Linked List/Project 1/LinkedList.js
@@ -0,0 +1,123 @@
+import { Node } from "./Node";
+
+export class LinkedList {
+  constructor () {
+    this.headNode = null;
+  }
+  append(value) {
+    const newNode = new Node(value);
+    if (!this.headNode) {
+      this.headNode = newNode;
+      return;
+    }
+    let current = this.headNode;
+    while (current.nextNode) {
+      current = current.nextNode;
+    }
+    current.nextNode = newNode;
+  }
+
+  prepend(value) {
+    const newNode = new Node(value, this.headNode);
+    this.headNode = newNode;
+  }
+
+  size() {
+    let count = 0;
+    let current = this.headNode;
+    while (current) {
+      count++;
+      current = current.nextNode;
+    }
+    return count;
+  }
+
+  head() {
+    return this.headNode;
+  }
+
+  tail() {
+    let current = this.headNode;
+    if (!current) return null;
+    while (current.nextNode) {
+      current = current.nextNode;
+    }
+    return current;
+  }
+
+  at(index) {
+    let current = this.headNode;
+    let count = 0;
+    while(current) {
+      if (count === index) return current;
+      count++;
+      current = current.nextNode;
+    }
+    return null;
+  }
+
+  pop() {
+    if (!this.headNode) return null;
+    if (!this.headNode.nextNode) {
+      this.headNode = null;
+      return;
+    }
+    let current = this.headNode;
+    while (current.nextNode && current.nextNode.nextNode) {
+      current = current.nextNode;
+    }
+    current.nextNode = null;
+  }
+
+  contains(value) {
+  let current = this.headNode;
+  while (current) {
+    if (current.value === value) return true;
+    current = current.nextNode;
+    }
+  return false;
+  }
+
+  find(value) {
+    let current = this.headNode;
+    let index = 0;
+    while (current) {
+      if (current.value === value) return index;
+      index++;
+      current = current.nextNode;
+    }
+    return null;
+  }
+
+  toString() {
+    let str = '';
+    let current = this.headNode;
+    while (current) {
+      str += `( ${current.value} ) -> `;
+      current = current.nextNode;
+    }
+    str += 'null';
+    return str;
+  }
+
+  insertAt(value, index) {
+    if (index === 0) {
+      this.prepend(value);
+      return;
+    }
+    let prev = this.at(index - 1);
+    if (!prev) return;
+    const newNode = new Node(value, prev.nextNode);
+    prev.nextNode = newNode;
+  }
+
+  removeAt(index) {
+    if (index === 0) {
+      this.headNode = this.headNode ? this.headNode.nextNode : null;
+      return;
+    }
+    let prev = this.at(index - 1);
+    if (!prev || !prev.nextNode) return;
+    prev.nextNode = prev.nextNode.nextNode;
+  }
+}
