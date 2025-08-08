b1614197814221a3a8f0d87c4a6d168773bc70a9
Charls Cabangon <charlsmercabangon@gmail.com>
2025-08-08 12:30:23 +0800

    complete balanced binary search tree

--- 03-intmd-javascript/04-a-bit-of-computer-science/04-binary-search-tree/binary-search-tree/index.js
+++ 03-intmd-javascript/04-a-bit-of-computer-science/04-binary-search-tree/binary-search-tree/index.js
@@ -5,10 +5,6 @@ const values = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
 const tree = Tree(values);
 
 
-// tree.insert(2);
-// tree.insert(400);
-// tree.deleteItem(2);
-
 prettyPrint(tree.getRoot());
 console.log(tree.findMin());
 console.log(tree.find(1));
@@ -24,3 +20,26 @@ tree.preOrderForEach(node => console.log(node.data));
 
 console.log("\nPost-order traversal:");
 tree.postOrderForEach(node => console.log(node.data));
+
+console.log(`\nheight: ${tree.height(8)}`);
+console.log(`\nheight: ${tree.height(4)}`);
+console.log(`\nheight: ${tree.height(1)}`);
+console.log(`\nheight: ${tree.height(3)}`);
+
+console.log(`\ndepth: ${tree.depth(8)}`);
+console.log(`\ndepth: ${tree.depth(4)}`);
+console.log(`\ndepth: ${tree.depth(1)}`);
+console.log(`\ndepth: ${tree.depth(3)}`);
+
+console.log(`\nis balanced: ${tree.isBalanced()}`);
+
+
+tree.insert(2);
+tree.insert(400);
+tree.insert(33);
+tree.deleteItem(2);
+tree.rebalance();
+
+prettyPrint(tree.getRoot());
+
+console.log(`\nis balanced: ${tree.isBalanced()}`);
