b0de21fc6c6677ec147ade0d78591ee0f6096aba
Charls Cabangon <charlsmercabangon@gmail.com>
2025-09-07 21:08:13 +0800

    start learning react

---
+++ 04-advanced-html-css/01-animations/01-button-hover/index.js
@@ -0,0 +1,11 @@
+
+const btn1 = document.getElementById("btn-1")
+const btn2 = document.getElementById("btn-2")
+
+btn1.addEventListener('click', () => {
+  console.log("button 1 clicked");
+});
+
+btn2.addEventListener('click', () => {
+  console.log("button 2 clicked");
+});
