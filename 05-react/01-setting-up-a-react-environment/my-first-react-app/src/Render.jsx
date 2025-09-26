bb03a12d94944e702a1e823b5d1279d6281badaa
Charls Cabangon <charlsmercabangon@gmail.com>
2025-09-26 22:08:18 +0800

    learn react testing

--- 05-react/01-setting-up-a-react-environment/my-first-react-app/src/Render.jsx
+++ 05-react/01-setting-up-a-react-environment/my-first-react-app/src/Render.jsx
@@ -1,29 +1,3 @@
-
-// function ListItem(props) {
-//   return <li>{props.animal}</li>
-// }
-
-// function List(props) {
-//   return (
-//     <ul>
-//       {props.animals.map((animal) => {
-//         return animal.startsWith("L") && <ListItem key={animal} animal={animal} />;
-//       })}
-//     </ul>
-//   );
-// }
-
-// export function App() {
-//   const animals = ["Lion", "Cow", "Snake", "Lizard"];
-
-//   return (
-//     <div>
-//       <h1>Animals: </h1>
-//       <List animals={animals} />
-//     </div>
-//   )
-// }
-
 function List(props) {
   if (!props.animals) {
     return <div>Loading...</div>;
--- 05-react/01-setting-up-a-react-environment/my-first-react-app/src/main.jsx
+++ 05-react/01-setting-up-a-react-environment/my-first-react-app/src/main.jsx
@@ -3,11 +3,9 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 // import App from './App.jsx'
 import { App } from './Render.jsx'
-import Greeting from './Greeting.jsx'
-import { Person, Count, Name } from './State.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Name />
+    <App />
   </StrictMode>,
 )
